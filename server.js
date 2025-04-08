782cc166c5254bfea2752d78fa7dbe75c5d49b45
John Gill <johnnymacsf@outlook.com>
2025-04-08 11:54:27 -0700

    Create API endpoints for essential login/signup functionality

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js"
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,7 +15,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
-
+// API endpoints
 app.get('/', (req, res) => res.send("API Working fine!"));
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
